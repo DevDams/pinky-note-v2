3290dd37f81265fff097c41358fd2dee531e0f66
DevDams <ad.aimedesire@gmail.com>
2021-07-05 14:44:38 +0000

    assign filterResult to notes

--- pages/index.vue
+++ pages/index.vue
@@ -242,7 +242,6 @@ export default {
       // check if note field is empty
       if (note.title !== '' || note.content !== '') {
         this.notes.push(note)
-        this.filterResult = this.notes
         this.filterResult.push(note)
         this.newNote = !this.newNote
         this.color = ''
